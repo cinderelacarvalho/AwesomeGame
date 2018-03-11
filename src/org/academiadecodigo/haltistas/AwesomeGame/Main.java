f374bcef8b97d8c328a1c89ce827ccfdea2058d5
cinderelacarvalho.com <cinderelacarvalho@gmail.com>
2018-03-11 00:07:47 +0000

    inteliJ file created

---
+++ src/org/academiadecodigo/haltistas/AwesomeGame/Main.java
@@ -0,0 +1,9 @@
+package org.academiadecodigo.haltistas.AwesomeGame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Beginning of AwesomeGame journey.");
+    }
+}
